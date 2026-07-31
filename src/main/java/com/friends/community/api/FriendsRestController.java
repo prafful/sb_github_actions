41599e6488fee574e9c6e1438a8765be567cd596
Prafful Daga <prraful@gmail.com>
2026-07-31 06:57:01 +0000

    cors

--- src/main/java/com/friends/community/api/FriendsRestController.java
+++ src/main/java/com/friends/community/api/FriendsRestController.java
@@ -24,17 +24,16 @@ import com.friends.community.model.Friend;
 import com.friends.community.service.FriendsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/friends/api/1.0")
+@CrossOrigin(origins = {
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+})
 public class FriendsRestController {
 
     private final FriendsService friendService;
